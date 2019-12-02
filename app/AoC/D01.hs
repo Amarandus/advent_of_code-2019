0f2f7cfb7b79b4fe96039c8ba9ae23115d0a589d
Amarandus <carl.schneider+github@ruhr-uni-bochum.de>
2019-12-02 02:27:26 +0100

    Add solution for day 1

--- app/AoC/D01.hs
+++ app/AoC/D01.hs
@@ -1,4 +1,35 @@
-module AoC.D01(d01main) where
+module AoC.D01(d01p1, d01p2) where
 
-d01main :: IO ()
-d01main = putStrLn "Day 1!"
+import Data.Maybe (catMaybes)
+import Text.Read (readMaybe)
+import Data.Function (fix)
+
+-- Fuel required to launch a given module is based on its mass.
+-- Specifically, to find the fuel required for a module, take its mass,
+-- divide by three, round down, and subtract 2.
+calcFuel :: Int -> Int
+calcFuel mass
+  | fuel >= 0 = fuel
+  | otherwise = 0
+  where
+    fuel = (floor $ (fromIntegral mass) / 3) - 2
+
+calcTotalFuel :: (Int -> Int) -> [Int] -> Int
+calcTotalFuel f = foldl (\acc v -> acc + f v) 0
+
+calcCorrectFuel :: Int -> Int
+calcCorrectFuel = sum . (takeWhile (> 0)) . (iterate calcFuel) . calcFuel
+
+d01p1 :: [String] -> IO ()
+d01p1 lines =
+  putStrLn $ "The total fuel required is " ++ show totalFuel
+  where
+    parsedLines = catMaybes $ map readMaybe lines
+    totalFuel = calcTotalFuel (calcFuel) parsedLines
+  
+d01p2 :: [String] -> IO ()
+d01p2 lines = 
+  putStrLn $ "The total fuel required is " ++ show totalFuel
+  where
+    parsedLines = catMaybes $ map readMaybe lines
+    totalFuel = calcTotalFuel (calcCorrectFuel) parsedLines
--- app/Main.hs
+++ app/Main.hs
@@ -9,17 +9,19 @@ import qualified Data.Map as Map
 
 import AoC.D01
 
-dayMap :: Map Int (IO ())
-dayMap = Map.fromList [(1, d01main)]
+dayMap :: Map Int ([String] -> IO (), [String] -> IO ())
+dayMap = Map.fromList [(1, (d01p1, d01p2))]
 
-data CmdFields = CmdFields {day :: Int}
+data CmdFields = CmdFields {day :: Int, part :: Int}
   deriving (Show, Data, Typeable)
 
-cmdField = CmdFields{day = 1 &= help "Selects the day of the challenge"}
+cmdField = CmdFields{day = 1 &= help "Selects the day of the challenge",
+                    part = 1 &= help "Selects the part of the day"}
 
 main :: IO ()
 main = do
   args <- cmdArgs cmdField
+  input <- getContents
   case Map.lookup (day args) dayMap of
-    Just x -> x
+    Just fun -> ((if (part args) == 1 then fst else snd) fun) $ lines input
     Nothing -> putStrLn "Please choose a day!"
